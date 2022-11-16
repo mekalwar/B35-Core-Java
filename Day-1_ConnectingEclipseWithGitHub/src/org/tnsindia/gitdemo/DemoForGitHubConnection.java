f7a120760e2d0f7a0068c32f738d4605b7dcaa80
Prince Kalwar <Prince Kalwar@192.168.56.1>
2022-11-16 19:00:52 +0530

    first commit

---
+++ Day-1_ConnectingEclipseWithGitHub/src/org/tnsindia/gitdemo/DemoForGitHubConnection.java
@@ -0,0 +1,8 @@
+package org.tnsindia.gitdemo;
+
+public class DemoForGitHubConnection {
+
+	public static void main (String[] args) {
+		System.out.println("Welcome to TNS india Foundation");
+	}
+}
