a3c3773f53012c26e8eb5de585b0eb7befd4b32c
Prince Kalwar <Prince Kalwar@192.168.56.1>
2022-12-02 19:30:42 +0530

    Day-10 first commit

---
+++ Day-10_Polymorphism,InterfaceAndWrapperClasses/src/org/tnsindia/polymorphismdemo/ConstructorOverloadingExecutor.java
@@ -0,0 +1,13 @@
+package org.tnsindia.polymorphismdemo;
+//Driver class
+public class ConstructorOverloadingExecutor {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Renault r=new Renault("RXE","Petrol");
+		@SuppressWarnings("unused")
+		Renault r1=new Renault("Kiger");
+
+	}
+
+}
