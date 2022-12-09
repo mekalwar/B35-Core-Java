bc3232336b415e71ebbb314e19dba74676a6b104
Prince Kalwar <Prince Kalwar@DESKTOP-H3PJDDR>
2022-12-09 18:53:27 +0530

    Day-13 first commit

---
+++ Day-13_ExceptionHandling/src/org/tnsindia/exceptiondemo/DemoOnThrowKeyword.java
@@ -0,0 +1,27 @@
+package org.tnsindia.exceptiondemo;
+//demo on throw keyword
+public class DemoOnThrowKeyword {
+	
+	//private data member
+	@SuppressWarnings("unused")
+	private String contents;
+
+
+
+	//method
+	public void display(String contents)
+	{
+		this.contents=contents;
+		if(contents.isEmpty())
+		{
+			throw new NullPointerException("Content is Empty");
+		}
+		else
+		{
+			System.out.println("Contents are: "+contents);
+		}
+	}
+
+	
+
+}
