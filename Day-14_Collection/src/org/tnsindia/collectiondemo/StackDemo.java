e1551902e036c34b2094efdd7dd2b8665c3fde98
Prince Kalwar <Prince Kalwar@DESKTOP-H3PJDDR>
2022-12-12 19:08:27 +0530

    Day-14 First Commit

---
+++ Day-14_Collection/src/org/tnsindia/collectiondemo/StackDemo.java
@@ -0,0 +1,24 @@
+package org.tnsindia.collectiondemo;
+
+import java.util.Stack;
+import java.util.Vector;
+
+@SuppressWarnings("unused")
+public class StackDemo {
+
+	public static void main(String[] args) {
+		Stack<String>obj=new Stack<>();
+		//insertion
+		obj.push("Shiwani");
+		obj.push("Pooja");
+		obj.push("Yogesh");
+		obj.push("Rajesh");
+		System.out.println(obj);
+		//remove-LIFO
+		obj.pop();
+		System.out.println(obj);
+
+
+	}
+
+}
